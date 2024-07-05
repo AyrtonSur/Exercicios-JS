82e860ef828da80c20352b89a1897dc3671da64c
Ayrton Surica <ayrtonsurica@gmail.com>
2024-07-05 20:46:07 -0300

    fix(questao3Extra): corrigido erro onde o array não estava sendo ordenado

--- questao3Extra.js
+++ questao3Extra.js
@@ -2,7 +2,17 @@
 var nums = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100)); 
 // "escolhendo" valores
 
-numsOrdenados = nums.toSorted()
+function comparaFunc(n1, n2){
+    if (n1 < n2){
+        return -1
+    } else if (n1 > n2){
+        return 1
+    } else {
+        return 0
+    }
+}
+
+numsOrdenados = nums.toSorted(comparaFunc)
 // criando array ordenado de valores
 
 console.log("Valores ordenados: " + numsOrdenados + " Valores: " + nums)
