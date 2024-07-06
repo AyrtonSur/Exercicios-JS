58354de65ac4f423058e02c247ea151399b699cc
Ayrton Surica <ayrtonsurica@gmail.com>
2024-07-05 21:44:50 -0300

    feat(questao5Extra): foi finalizada a questao5Extra

--- questao5Extra.js
+++ questao5Extra.js
@@ -23,5 +23,47 @@ const booksByCategory = [ {
             author: "Stephen R. Covey"
         }]
     }
-];
+]
+
+console.log("Numero de categorias: " + booksByCategory.length)
+//imprime número de categorias
+
+for (category of booksByCategory){
+    console.log("Numero de livros na categoria "+ category.category + ": "  + category.books.length)
+} // imprime número de livros, para cada categoria
+
+for (category of booksByCategory){
+    var count = 0
+    for (book of category.books){
+        var autores = book.author.split(" e " || ",")
+        count += autores.length
+    }
+    console.log("Numero de autores na categoria " + category.category + " é:" + count)
+} // imprime o número de autores em cada categoria de livros
+
+for (category of booksByCategory){
+    for (book of category.books){
+        var bool = false
+        var autores = book.author.split(" e " || ",")
+        for (author of autores){ // verifica se autor é Augusto
+            if (author == "Augusto Cury") bool = true
+        }
+        if (bool == true) console.log("Livros escritos por Augusto Cury: " + book.title)
+    }
+} // imprime os livros do autor Augusto
+
+function authorBooks(author, categoryAndBooks){
+    listaDeLivros = []
+    for (category of categoryAndBooks){
+        for (book of category.books){
+            var bool = false
+            var autores = book.author.split(" e " || ",")
+            for (autor of autores){ // verifica se autor é Augusto
+                if (autor == author) bool = true
+            }
+            if (bool == true) listaDeLivros.push(book)
+        }
+    } //transformando a ação feita em cima em uma função que recebe o nome do autor e devolve os livros que ele escreveu
+    return listaDeLivros
+} // função retorna array de livros
 
